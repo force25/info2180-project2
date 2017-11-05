e6cfcd3de533d5b6e03bf4b54dd6143c85fb9202
Jonathan <jonforce52@hotmail.com>
2017-11-04 23:31:21 -0500

    changed variable name and delete unncessesary code

--- fifteen.js
+++ fifteen.js
@@ -1,35 +1,35 @@
 var emptyTop="400px";
 var emptyLeft="400px";
 var result;
-//console.log(parseInt(l[j].innerHTML));
+//console.log(parseInt(inner[j].innerHTML));
 window.onload = function(){	
-var i = document.getElementById("puzzlearea")
-var l = i.getElementsByTagName("DIV");
+var puzzle = document.getElementById("puzzlearea")
+var inner = puzzle.getElementsByTagName("DIV");
 var width = 0;
 var height = 0;
 
-i.style.setProperty("top", "-100px");
+createPuzzle();
+puzzle.style.setProperty("top", "-100px");
 changeTop();
 changeLeft();
 Grid();
 getall();
-for (var j = 0; j<l.length;j++){
-  l[j].setAttribute("class","puzzlepiece");
-	}
 }
+
+function createPuzzle(){
+var puzzle = document.getElementById("puzzlearea");
+var inner = puzzle.getElementsByTagName("DIV");
+for (var j = 0; j<inner.length;j++){
+  inner[j].setAttribute("class","puzzlepiece");
+		}
+	}
 
 function getall(){
 var p
-var i = document.getElementById("puzzlearea").getElementsByTagName("DIV");
-			
-			//var d=i[6+1];
-			//console.log(d);
-	for (var j = 0; j<i.length;j++){
-		i[j].addEventListener("click", function(){
+var puzzle = document.getElementById("puzzlearea").getElementsByTagName("DIV");
+	for (var j = 0; j<puzzle.length;j++){
+		puzzle[j].addEventListener("click", function(){
 			 p = parseInt(this.innerHTML-1);
-			//ifMovable();
-			//this.style.borderColor="red";
-			//var p = parseInt(i[j].innerHTML-1);
 			ifmoveable(p);
 		if(result=="true"){
 			move(p);
@@ -40,20 +40,31 @@ var i = document.getElementById("puzzlearea").getElementsByTagName("DIV");
 		});
 	}
 }
+
+function changeHover(){
+	var puzzle = document.getElementById("puzzlearea").getElementsByTagName("DIV");
+	for (var j = 0; j<puzzle.length;j++){
+		puzzle[j].addEventListener("mouseover", function(){
+		this.style.borderColor = "red";
+		this.style.textDecoration="underline";
+		this.style.color="green";
+});
+	}
+}
 function ifmoveable(piece){
-   var i = document.getElementById("puzzlearea").getElementsByTagName("DIV");
+   var puzzle = document.getElementById("puzzlearea").getElementsByTagName("DIV");
    var topEmpty  = parseInt(emptyTop);
    var leftEmpty  =  parseInt(emptyLeft);
-		if(parseInt(i[piece].style.left)+100 ==leftEmpty && parseInt(i[piece].style.top) == topEmpty){
+		if(parseInt(puzzle[piece].style.left)+100 ==leftEmpty && parseInt(puzzle[piece].style.top) == topEmpty){			
 			result = "true";
    }
-   else if(parseInt(i[piece].style.left)-100 ==leftEmpty && parseInt(i[piece].style.top) == topEmpty){
+   else if(parseInt(puzzle[piece].style.left)-100 ==leftEmpty && parseInt(puzzle[piece].style.top) == topEmpty){
 			result = "true";
    }
-   else if(parseInt(i[piece].style.left) ==leftEmpty && parseInt(i[piece].style.top)+100== topEmpty){
+   else if(parseInt(puzzle[piece].style.left) ==leftEmpty && parseInt(puzzle[piece].style.top)+100== topEmpty){
 			result = "true";
    }
-   else if(parseInt(i[piece].style.left)==leftEmpty && parseInt(i[piece].style.top)-100 == topEmpty){
+   else if(parseInt(puzzle[piece].style.left)==leftEmpty && parseInt(puzzle[piece].style.top)-100 == topEmpty){
 			result = "true";
    }
    else{
@@ -61,57 +72,56 @@ function ifmoveable(piece){
    }
 }
 function move(piece){
-	var i = document.getElementById("puzzlearea")
-	var l = i.getElementsByTagName("DIV")[piece];
-	temp=l.style.top;
+	var puzzle = document.getElementById("puzzlearea")
+	var inner = puzzle.getElementsByTagName("DIV")[piece];
+	temp=inner.style.top;
 	console.log(temp);
-	l.style.top= emptyTop;
+	inner.style.top= emptyTop;
 	emptyTop= temp;
-	temp=l.style.left;
-	l.style.left= emptyLeft;
+	temp=inner.style.left;
+	inner.style.left= emptyLeft;
 	emptyLeft= temp;
 }		
-		
 
 function changeTop(){
 var width = 0;
-var i = document.getElementById("puzzlearea").getElementsByTagName("DIV");
-for (var j = 0; j<i.length;j++){
+var puzzle = document.getElementById("puzzlearea").getElementsByTagName("DIV");
+for (var j = 0; j<puzzle.length;j++){
    if (j%4 == 0){
 	 width = width+100;
    }
-   i[j].style.setProperty("top", width+"px");
+   puzzle[j].style.setProperty("top", width+"px");
 	}
 }
 function changeLeft(){
-var i = document.getElementById("puzzlearea").getElementsByTagName("DIV");
+var puzzle = document.getElementById("puzzlearea").getElementsByTagName("DIV");
 var height = 0;
-	for (var j = 0; j<i.length;j++){
+	for (var j = 0; j<puzzle.length;j++){
 	 height = height+100;
 	 if (j%4 == 0){
 	 height = 100;
    }
-	 i[j].style.setProperty("left", height+"px");
+	 puzzle[j].style.setProperty("left", height+"px");
    }
 }
 
 function Grid(){
-var i = document.getElementById("puzzlearea")
-var l = i.getElementsByTagName("DIV");
-//l[15].style.background= "none";
-l[14].style.backgroundPosition = " -200px -300px";
-l[13].style.backgroundPosition = " -100px -300px";
-l[12].style.backgroundPosition = "  0px -300px";
-l[11].style.backgroundPosition = " -300px -200px";
-l[10].style.backgroundPosition = " -200px -200px";
-l[9].style.backgroundPosition  = " -100px -200px";
-l[8].style.backgroundPosition  = "  0px -200px";
-l[7].style.backgroundPosition  = " -300px -100px";
-l[6].style.backgroundPosition  = " -200px -100px";
-l[5].style.backgroundPosition  = " -100px -100px";
-l[4].style.backgroundPosition  = "  0px -100px";
-l[3].style.backgroundPosition  = " -300px 0px";
-l[2].style.backgroundPosition  = " -200px 0px";
-l[1].style.backgroundPosition  = " -100px 0px";
-l[0].style.backgroundPosition  = " 0px 0px";
+var puzzle = document.getElementById("puzzlearea")
+var inner = puzzle.getElementsByTagName("DIV");
+//inner[15].style.background= "none";
+inner[14].style.backgroundPosition = " -200px -300px";
+inner[13].style.backgroundPosition = " -100px -300px";
+inner[12].style.backgroundPosition = "  0px -300px";
+inner[11].style.backgroundPosition = " -300px -200px";
+inner[10].style.backgroundPosition = " -200px -200px";
+inner[9].style.backgroundPosition  = " -100px -200px";
+inner[8].style.backgroundPosition  = "  0px -200px";
+inner[7].style.backgroundPosition  = " -300px -100px";
+inner[6].style.backgroundPosition  = " -200px -100px";
+inner[5].style.backgroundPosition  = " -100px -100px";
+inner[4].style.backgroundPosition  = "  0px -100px";
+inner[3].style.backgroundPosition  = " -300px 0px";
+inner[2].style.backgroundPosition  = " -200px 0px";
+inner[1].style.backgroundPosition  = " -100px 0px";
+inner[0].style.backgroundPosition  = " 0px 0px";
 }
