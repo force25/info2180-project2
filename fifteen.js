6be0f3ed9d8604d88a0229d3dcc66a3ce2e59af1
Jonathan <jonforce52@hotmail.com>
2017-11-05 16:50:58 -0500

    extra feature change background done

--- fifteen.js
+++ fifteen.js
@@ -6,23 +6,116 @@ var puzzle = document.getElementById("puzzlearea")
 var inner = puzzle.getElementsByTagName("DIV");
 var width = 0;
 var height = 0;
-
+var btn1 = document.createElement("Button");
+var btn2 = document.createElement("Button");
+var btn3 = document.createElement("Button");
+var btn4 = document.createElement("Button");
+var btn5 = document.createElement("Button");
+var main = document.getElementById("controls");
+main.appendChild(btn1);
+main.appendChild(btn2);
+main.appendChild(btn3);
+main.appendChild(btn4);
+main.appendChild(btn5);
+var text1= document.createTextNode("Background1");
+var text2= document.createTextNode("Background2");
+var text3= document.createTextNode("Background3");
+var text4= document.createTextNode("Background4");
+var text5= document.createTextNode("Background5");
+btn1.appendChild(text1);
+btn2.appendChild(text2);
+btn3.appendChild(text3);
+btn4.appendChild(text4);
+btn5.appendChild(text5);
+var id1 = btn1.setAttribute("id","Default");
+var id2 = btn2.setAttribute("id","button2");
+var id3 = btn3.setAttribute("id","button3");
+var id4 = btn4.setAttribute("id","button4");
+var id5 = btn5.setAttribute("id","button5");
 
 createPuzzle();
 puzzle.style.setProperty("top", "-100px");
+puzzle.style.setProperty("left", "-70px");
 changeTop();
 changeLeft();
 Grid();
 changeHover();
 getall();
 shuffleClick();
+changeBack();
 }
+
+function changeBack(){
+var bg1 = document.getElementById("Default");
+var bg2 = document.getElementById("button2");
+var bg3 = document.getElementById("button3");
+var bg4 = document.getElementById("button4");
+var bg5 = document.getElementById("button5");
+var b;
+	bg1.addEventListener("click",function(){
+		b=bg1.innerHTML;
+		console.log(b)
+		decideImage(b);
+			});
+	bg2.addEventListener("click",function(){
+			b=bg2.innerHTML;
+			console.log(b)
+			decideImage(b);
+			});
+	bg3.addEventListener("click",function(){
+			b=bg3.innerHTML;
+			console.log(b)
+			decideImage(b);
+			});
+	bg4.addEventListener("click",function(){
+			b=bg4.innerHTML;
+			console.log(b)
+			decideImage(b);
+			});
+	bg5.addEventListener("click",function(){
+			b=bg5.innerHTML;
+			console.log(b)
+			decideImage(b);
+			});
+}
+function decideImage(bg){
+var puzzle = document.getElementById("puzzlearea")
+var inner = puzzle.getElementsByTagName("DIV");
+	if(bg== "Background1"){
+		for (var j = 0; j<inner.length;j++){
+		inner[j].style.backgroundImage = "url('https://raw.githubusercontent.com/force25/info2180-project2/gh-pages/background.jpg')";
+				}
+		}
+		else if(bg == "Background2"){
+		for (var j = 0; j<inner.length;j++){
+		inner[j].style.backgroundImage = "url('https://raw.githubusercontent.com/force25/info2180-project2/gh-pages/rin.jpg')";
+				}	
+		}
+		else if(bg == "Background3"){
+		for (var j = 0; j<inner.length;j++){
+		inner[j].style.backgroundImage = "url('https://raw.githubusercontent.com/force25/info2180-project2/gh-pages/saber.jpg')";
+				}
+		}
+		else if(bg == "Background4"){
+		for (var j = 0; j<inner.length;j++){
+		inner[j].style.backgroundImage = "url('https://raw.githubusercontent.com/force25/info2180-project2/gh-pages/sakura.jpg')";
+				}	
+		}
+		else if(bg == "Background5"){
+		for (var j = 0; j<inner.length;j++){
+		inner[j].style.backgroundImage = "url('https://raw.githubusercontent.com/force25/info2180-project2/gh-pages/scathach.jpg')";
+				}			
+		}
+		else{return console.log("d");}
+}
+
 
 function createPuzzle(){
 var puzzle = document.getElementById("puzzlearea");
 var inner = puzzle.getElementsByTagName("DIV");
 for (var j = 0; j<inner.length;j++){
   inner[j].setAttribute("class","puzzlepiece");
+  
 		}
 	}
 
